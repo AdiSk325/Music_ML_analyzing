123bb31cb7612f325db1695cf9c060e2f223d417
AdiSk325 <skutnik.adrian@gmail.com>
2020-04-16 20:37:51 +0200

    libraries import

--- utils/AudioConnection.py
+++ utils/AudioConnection.py
@@ -1,4 +1,19 @@
-from utils import *
+import pandas as pd
+import numpy as np
+import sklearn
+
+import matplotlib.pyplot as plt
+
+import soundfile as sf
+import librosa
+import librosa.display
+
+import pygame, pygame.sndarray, pyaudio
+import wave
+import time
+import sys
+
+import os
 
 class AudioConn():
 
