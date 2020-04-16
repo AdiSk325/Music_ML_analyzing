4f4480ef91bd32d691b93d8c1dde45c65b72b598
AdiSk325 <skutnik.adrian@gmail.com>
2020-04-16 20:34:04 +0200

    modify utils dir

--- utils/AudioConnection.py
+++ utils/AudioConnection.py
@@ -1,4 +1,4 @@
-from MA_utils import *
+from utils import *
 
 class AudioConn():
 
---
+++ utils/__init__.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+import matplotlib.pyplot as plt
+
+import soundfile as sf
+import librosa
+import librosa.display
+
+import pygame, pygame.sndarray, pyaudio
+import wave
+import time
+import sys
+
+import os
